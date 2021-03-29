55551a5ec8f5d115f8cf32154a7b457be1c027cd
jdshez <jd_shez@protonmail.com>
2021-03-29 23:02:29 +0100

    fix review imgs

--- observer.js
+++ observer.js
@@ -5,15 +5,38 @@ const sectionOptions = {
     rootMargin:"-50px 0px 0px 0px"
 };
 
-const observer = new IntersectionObserver(function(entries, observer) {
+const observer1 = new IntersectionObserver(function(entries, observer) {
     entries.forEach(entry => {
-        //console.log(entry.target);
-        if (!entry.isIntersecting) {
-            header.classList.add('scrolled');
-        } else if (entry.isIntersecting) {
-            header.classList.remove('scrolled');
+        if (screen.width > 900) {
+            if (!entry.isIntersecting) {
+                header.classList.add('hide');
+            } else if (entry.isIntersecting) {
+                header.classList.remove('hide');
+            }
+        } else {
+            if (!entry.isIntersecting) {
+                header.classList.add('scrolled');
+            } else if (entry.isIntersecting) {
+                header.classList.remove('scrolled');
+            }
         }
     })
 }, sectionOptions);
 
-observer.observe(section);
+observer1.observe(section);
+
+
+const fab = document.querySelector(".fixed-action-btn");
+const observer2 = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        if (screen.width > 900) {
+            if (entry.isIntersecting) {
+                fab.classList.add('hide');
+            } else if (!entry.isIntersecting) {
+                fab.classList.remove('hide');
+            }
+        } else fab.classList.add('hide');
+    })
+}, sectionOptions);
+
+observer2.observe(section);
